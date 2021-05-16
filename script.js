99f087055489917cdbf46ea10b42d191ac55787d
lela <baidoshvililela3@gmail.com>
2021-05-16 03:12:01 -0700

    add project

---
+++ script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+//alert('Hello');
+
+//const result = confirm("Are you here?")
+//console.log(result);
+
+//const answer = +prompt("are you 18?", "18");
+//console.log(answer + 5);
+
+//const answers = [];
+
+//answers[1] = prompt('what is your lastname?', '');
+//answers[2] = prompt('how old are you?', '');
+
+//console.log(typeof(answers));
+
+//const category ='toys';
+
+//console. log('https://someurl.com/${category}/5');
+
+//const user = "ivan";
+
+//alert (`privet, ${user}`);
+
+//console.log ('arr' + " - object");
+//console.log (4 + +"5");
+//let incr = 10,
+//	decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+console.log(2*2 == 8);
